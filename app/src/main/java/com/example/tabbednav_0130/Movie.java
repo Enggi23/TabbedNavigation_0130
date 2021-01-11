09ff88953fb3629b77d03d3694d87dd832840708
Enggi23 <wahyu.0602@students.amikom.ac.id>
2021-01-12 03:30:02 +0700

    TabbedNavigation_0130

---
+++ app/src/main/java/com/example/tabbednav_0130/Movie.java
@@ -0,0 +1,32 @@
+package com.example.tabbednav_0130;
+
+import java.util.List;
+
+public class Movie {
+    private String title;
+    private String year;
+
+    public Movie(String title, String year) {
+        this.title = title;
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+}
+
+
+
